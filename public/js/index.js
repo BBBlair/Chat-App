f65097739f63d16295bcecd51c705b7286929414
Blair Xiao <blairxiao@AdminisatorsMBP.home>
2019-01-12 14:06:40 -0500

    Emit newMsgon createMsg

--- public/js/index.js
+++ public/js/index.js
@@ -3,17 +3,17 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to Server');
 
-  // This happens when the client and server connect
+  //This happens when the client and server connect
   // socket.emit('createEmail', {
   //   to: 'jen@example.com',
   //   text: 'Hey'
   // });
 
   //This happens when the client and server connect
-  socket.emit('createMsg', {
-    from: 'Blair',
-    text: 'It works for me'
-  });
+  // socket.emit('createMsg', {
+  //   from: 'Blair',
+  //   text: 'It works for me'
+  // });
 
 });
 
--- server/server.js
+++ server/server.js
@@ -14,18 +14,24 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMsg', {
-    from: 'John',
-    text: 'See you then',
-    createAt: 123123
-  });
+  // socket.emit('newMsg', {
+  //   from: 'John',
+  //   text: 'See you then',
+  //   createAt: 123123
+  // });
 
-    // socket.on('createEmail', (newEmail) => {
+  // socket.on('createEmail', (newEmail) => {
     //   console.log('createEmail', newEmail);
     // });
 
+//socket.emit() only emits an event to one user but io.emit() does it to every connection
   socket.on('createMsg', (msg) => {
     console.log('createMsg', msg);
+    io.emit('newMsg', {
+      from: msg.from,
+      text: msg.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
